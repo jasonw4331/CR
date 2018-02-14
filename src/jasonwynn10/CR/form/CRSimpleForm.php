075b545cd87e054c78f1a9b0d96bea3e538e6019
JackNoordhuis <daswaggamer@gmail.com>
2018-02-14 11:19:47 +1100

    Fix forms
    Gotta love using plugins that provide an 'API' :D

--- src/jasonwynn10/CR/form/CRSimpleForm.php
+++ src/jasonwynn10/CR/form/CRSimpleForm.php
@@ -1,29 +1,34 @@
 <?php
+
 declare(strict_types=1);
+
 namespace jasonwynn10\CR\form;
 
 use jojoe77777\FormAPI\SimpleForm;
 use pocketmine\Player;
 
 abstract class CRSimpleForm extends SimpleForm implements CRForm {
+
 	public function __construct() {
 		$formApi = CRFormUtils::getFormAPI();
 		$formApi->formCountBump();
 		parent::__construct($formApi->formCount, [$this, "onSubmit"]);
+		$formApi->forms[$this->id] = $this;
 	}
+
+	protected abstract function setup(Player $player) : void;
 
 	public function sendToPlayer(Player $player) : void {
 		$this->setup($player);
 		parent::sendToPlayer($player);
 	}
 
-	protected abstract function setup(Player $player) : void;
-
 	/**
 	 * @param Player $player
-	 * @param mixed $data
+	 * @param mixed  $data
 	 */
 	public function onSubmit(Player $player, $data) : void {
 
 	}
+
 }
